18976e1e334ff1598b21653936f202c397985e37
tajikwizard <abdulazizcodeshere@gmail.com>
2024-07-24 10:12:13 +0500

    refactoring

--- day24/index.js
+++ day24/index.js
@@ -1,3 +1,4 @@
 import './src/components/Error.js';
 import './src/components/Search.js';
 import  './src/components/JobList.js';
+import './src/components/JobDetails.js';
--- day24/src/components/Search.js
+++ day24/src/components/Search.js
@@ -9,6 +9,7 @@ import {
 
 import renderErrorMessage from './Error.js'
 import renderSpinner from './Spinner.js'
+import { renderJobList } from './JobList.js';
 
 // -- SEARCH COMPONENT --
 const submitHandler = event => {
@@ -56,29 +57,7 @@ const submitHandler = event => {
             numberEl.textContent = jobItems.length;
 
             // render job items in search job list
-            jobItems.slice(0, 7).forEach(jobItem => {
-                const newJobItemHTML = `
-                    <li class="job-item">
-                        <a class="job-item__link" href="${jobItem.id}">
-                            <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                            <div class="job-item__middle">
-                                <h3 class="third-heading">${jobItem.title}</h3>
-                                <p class="job-item__company">${jobItem.company}</p>
-                                <div class="job-item__extras">
-                                    <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobItem.salary}</p>
-                                    <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
-                                </div>
-                            </div>
-                            <div class="job-item__right">
-                                <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                                <time class="job-item__time">${jobItem.daysAgo}d</time>
-                            </div>
-                        </a>
-                    </li>
-                `;
-                jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
-            });
+            renderJobList(jobItems);
         })
         .catch(error => console.log(error));
 };
